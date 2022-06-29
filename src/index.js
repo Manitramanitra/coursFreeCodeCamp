bbc62001f3fd2a1978648c296ff27cef0fd20236
manitra <manitra@manitra.manitra>
2022-06-29 08:52:50 +0300

    petit algo

--- src/index.js
+++ src/index.js
@@ -8,4 +8,22 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
+// let temp = new Array();
+// let tab  = new Array();
+// const name = ['Manitra','Alice','Manoa','Iatrika','Gregoire'];
+// const newName = name.filter((item)=>{
+//   const tabvoyelle = ['a','e','i','o','y','u']
+//   for(let i=0; i<tabvoyelle.length; i++){
+//     if(item[0].toLocaleLowerCase().includes(tabvoyelle[i])){
+//       return item
+//     }
+//   }
+// })
+// console.log(newName);
+let temp = new Array();
+let tab  = new Array();
+const name = ['Manitra','Alice','Manoa','Iatrika','Gregoire'];
+const newName = name.filter((item)=>{
+  return item[0].toLocaleUpperCase()
+})
+console.log(newName);
