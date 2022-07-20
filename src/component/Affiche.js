1f5ac3ac3404aca457db2aae76ceae3278c3e5c5
Manitramanitra <randiluc@gmail.com>
2022-07-20 14:10:28 +0300

    fin du carte avec utilisation de map

--- src/component/Affiche.js
+++ src/component/Affiche.js
@@ -14,9 +14,6 @@ const Affiche = () => {
             description={item.description}
         />)
     })
-    console.log("toto")
-    console.log(datasAffiche)
-    console.log("tata")
     return (
         <>
             {datasAffiche}
